c0ee39663ad87622f5030a4b67352db02dfa8c21
JesperS-VR <ext-jesper.sundqvist@vr.fi>
2023-01-20 14:52:58 +0200

    Made the Backend read the text file from the web

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
-
+const http = require("http");
 var fs = require('fs');
 
+const file = fs.createWriteStream("file.txt");
+
+http.get("http://tuftuf.gambitlabs.fi/feed.txt", response => {
+    response.pipe(file);
+});
+
 try {  
-    var data = fs.readFileSync('textfile.txt', 'utf8');
+    var data = fs.readFileSync('file.txt', 'utf8');
     console.log(data.toString());    
 } catch(e) {
     console.log('Error:', e.stack);
 }
 
+
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
+    
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000")});
